04c5d4beed8c8b3ee1579588bc62a421df70ff62
Charlie Zhao <zhaocharlie99@gmail.com>
2022-05-16 15:53:09 -0700

    add state machine

---
+++ Source/FishFlock/Animation/FishGroupAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "FishGroupAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FISHFLOCK_API UFishGroupAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
